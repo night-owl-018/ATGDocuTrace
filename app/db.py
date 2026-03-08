16e7dd76cf80221752d8f43027a047f8a07c00c7
nightowl <34909304+night-owl-018@users.noreply.github.com>
2026-03-08 14:32:44 -0700

    Set up SQLAlchemy for database connection

--- app/db.py
+++ app/db.py
@@ -1,1 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
+DATABASE_URL = "sqlite:///data/database/jobs.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
